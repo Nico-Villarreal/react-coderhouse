3f1c0ff23f09b1fdaa0150ef77da210a56c9b240
Nicolas Villarreal <105127312+Nico-Villarreal@users.noreply.github.com>
2023-02-05 21:58:57 -0300

    entrega final v1.3

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,49 @@
 import { useContext } from "react";
 import { CartContext } from "../components/Context/CartContext";
+import { collection, addDoc, getFirestore } from "firebase/firestore";
+import { useState } from "react";
 
 const Cart = () => {
   const { cart, deleteItem } = useContext(CartContext);
+  const [ order, setOrder ] = useState({});
+
+  if (cart.length === 0) {
+    return (
+      <h1 className="m-5 p-5 text-center"> No hay elementos en el carrito </h1>
+    );
+  }
+
+  const createOrder = () => {
+    console.log("hola");
+    const dataBase = getFirestore();
+    const querySnapshot = collection(dataBase, "orders");
+
+    const newOrder ={
+      buyer: {
+        name: "diego",
+        phone: "+541132556",
+        email: "test@test.com",
+      },
+      item: cart.map((product) => {
+        const { name, price, id} = product;
+        return {
+          name,
+          price,
+          id
+        }
+      }),
+      total: cart.reduce((productPass, productNow) => productPass + productNow.price * productNow.quantity, 0),
+    }
+    setOrder(newOrder);
+
+    addDoc(querySnapshot, order)
+      .then((response) => {
+        console.log(response.id);
+        alert("orden creada con exito, id " + response.id);
+      })
+      .catch((error) => console.log(error));
+  };
+
   console.log(cart);
   return (
     <div>
@@ -27,7 +68,7 @@ const Cart = () => {
             <div className="d-flex">
               <button
                 onClick={() => deleteItem(product.id)}
-                className="rounded fw-bolder h-50 p-2"
+                className="rounded fw-bolder h-50 p-2 m-2"
               >
                 Eliminar producto
               </button>
@@ -35,6 +76,14 @@ const Cart = () => {
           </div>
         </div>
       ))}
+      <div>
+        <button
+          onClick={createOrder}
+          className="rounded fw-bolder h-50 p-2 m-2"
+        >
+          Crear Compra
+        </button>
+      </div>
     </div>
   );
 };
